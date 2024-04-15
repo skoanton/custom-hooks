e32e2311ce7f07234bac114de528b77615bd00e6
skoanton <antonhakanssonn@gmail.com>
2024-04-15 14:39:41 +0200

    toggle klar

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ function App() {
 
   return (
     <>
-        {/* <HooksDemo startNumber={10} minNumber={-20} maxNumber={20} stepValue={5}/>
-        <HooksDemo startNumber={20} minNumber={-50} maxNumber={50} stepValue={10}/>
-        <HooksDemo startNumber={0} minNumber={-100} maxNumber={100} stepValue={20}/> */}
 
-       {/*  <CatInfoBox/> */}
-       
+      <h1>Counter</h1>
+        <HooksDemo startNumber={10} minNumber={-20} maxNumber={20} stepValue={5}/>
+        <HooksDemo startNumber={20} minNumber={-50} maxNumber={50} stepValue={10}/>
+        <HooksDemo startNumber={0} minNumber={-100} maxNumber={100} stepValue={20}/> 
+      <h1>FetchAPI</h1>
+       <CatInfoBox/>
+      <h1>Toggle</h1>
         <ToggleBox/>
     </>
   )
